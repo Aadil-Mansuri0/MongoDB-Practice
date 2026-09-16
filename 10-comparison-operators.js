5a5b19e7adb70cb322ef0992c21b86cb95d79f1a
Aadil Mansuri <mansuriaadil978@gmail.com>
2026-09-16 13:33:01 +0530

    Complete comparison operators practice

--- 10-comparison-operators.js
+++ 10-comparison-operators.js
@@ -1,21 +1,13 @@
 // Part 04: Comparison Operators
+// Demonstrates MongoDB comparison query operators.
 
 use("sample02");
 
-// Marks greater than 80
 db.students.find({ marks: { $gt: 80 } });
-
-// Marks greater than or equal to 85
 db.students.find({ marks: { $gte: 85 } });
-
-// Marks less than 70
 db.students.find({ marks: { $lt: 70 } });
-
-// Marks less than or equal to 70
 db.students.find({ marks: { $lte: 70 } });
-
-// Marks equal to 85
 db.students.find({ marks: { $eq: 85 } });
-
-// Department not equal to CSE
 db.students.find({ department: { $ne: "CSE" } });
+db.students.find({ department: { $in: ["CSE", "AI"] } });
+db.students.find({ department: { $nin: ["ECE", "ME"] } });
